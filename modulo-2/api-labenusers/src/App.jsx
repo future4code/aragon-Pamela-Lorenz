2034016994fdba9c80b3751ce8d2c822b17112f3
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-05-04 17:52:05 -0300

    trabalho

--- modulo-2/api-labenusers/src/App.jsx
+++ modulo-2/api-labenusers/src/App.jsx
@@ -1,45 +1,172 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import SignUp from "./components/SignUp.js";
+import Users from "./components/Users.js";
+import UserDetail from "./components/UserDetail.js";
+import UserEdit from "./components/UserEdit.js";
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const axiosConfig = {
+  headers: {
+    Authorization: "severo"
+  }
+};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+export default class App extends React.Component {
 
-export default App
+  state = {
+    usersList: [],
+    userDetail: {},
+    name: "",
+    email: "",
+    isEditing: false
+  }
+
+  componentDidMount() {
+    this.getUsersList();
+  };
+
+  handleNameChange = (event) => {
+    const newNameValue = event.target.value;
+
+    this.setState({ name: newNameValue });
+  };
+
+  handleEmailChange = (event) => {
+    const newEmailValue = event.target.value;
+
+    this.setState({ email: newEmailValue });
+  };
+
+  changeToEdit = () => {
+    this.setState({ isEditing: !this.state.isEditing })
+  }
+
+  getUsersList = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        axiosConfig
+      )
+      .then(response => {
+        this.setState({ usersList: response.data });
+      });
+  };
+
+  getUserDetail = (userId) => {
+    axios
+      .get(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId
+        }`,
+        axiosConfig
+      )
+      .then(response => {
+        this.setState({ userDetail: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleCreateUser = () => {
+    const body = {
+      name: this.state.name,
+      email: this.state.email
+    };
+
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        body,
+        axiosConfig
+      )
+      .then(() => {
+        alert(`Usuário ${this.state.name} criado com sucesso!`);
+        this.setState({ name: "", email: "" });
+        this.getUsersList()
+      })
+      .catch(error => {
+        alert("Erro ao criar o usuário");
+        console.log(error);
+      });
+  };
+
+  handleEditUser = (userId) => {
+    const body = {
+      name: this.state.name,
+      email: this.state.email
+    };
+
+    if (body.name && body.email) {
+      axios
+        .put(
+          `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`,
+          body,
+          axiosConfig
+        )
+        .then(() => {
+          alert(`Usuário alterado com sucesso!`);
+          this.setState({ name: "", email: "", userDetail: {}, isEditing: !this.state.isEditing });
+          this.getUsersList();
+          this.getUserDetail();
+        })
+        .catch((error) => {
+          alert("Erro ao alterar o usuário");
+          console.log(error);
+        });
+    } else {
+      alert("Os campos de nome e email devem ser preenchidos!");
+    };
+  };
+
+  handleUserDeletion = (userId) => {
+    if (window.confirm("Tem certeza que deseja apagar o usuário?")) {
+      axios
+        .delete(
+          `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`,
+          axiosConfig
+        )
+        .then(() => {
+          alert("Usuário apagado com sucesso!");
+          this.getUsersList();
+        })
+        .catch(e => {
+          alert("Erro ao apagar usuário");
+        });
+    }
+  };
+
+  render() {
+    return (
+      <main>
+        <SignUp
+          name={this.state.name}
+          email={this.state.email}
+          handleNameChange={this.handleNameChange}
+          handleEmailChange={this.handleEmailChange}
+          handleCreateUser={this.handleCreateUser}
+        />
+        <hr />
+        <Users
+          usersList={this.state.usersList}
+          getUserDetail={this.getUserDetail}
+          handleUserDeletion={this.handleUserDeletion}
+        />
+        <hr />
+        {this.state.isEditing ?
+          <UserEdit
+            name={this.state.name}
+            email={this.state.email}
+            userDetail={this.state.userDetail}
+            handleNameChange={this.handleNameChange}
+            handleEmailChange={this.handleEmailChange}
+            handleEditUser={this.handleEditUser}
+            changeToEdit={this.changeToEdit}
+          /> :
+          <UserDetail
+            userDetail={this.state.userDetail}
+            changeToEdit={this.changeToEdit}
+          />}
+      </main>
+    );
+  };
+};
