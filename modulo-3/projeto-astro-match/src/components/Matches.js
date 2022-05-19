e5e850883e9fa5d3b8252ea4cc2d0fc6e5de1e5e
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-05-19 09:15:44 -0300

    primeira verssão

---
+++ modulo-3/projeto-astro-match/src/components/Matches.js
@@ -0,0 +1,10 @@
+import react, { useState } from "react"
+export default function Matches() {
+    return (
+        <section>
+            <h1>
+                pagina de matches
+            </h1>
+        </section>
+    )
+}
---
+++ modulo-3/projeto-astro-match/src/components/Perfil.js
@@ -0,0 +1,10 @@
+import react, { useState } from "react"
+export default function Perfil() {
+    return (
+        <section>
+            <h1>
+                pagina de perfil
+            </h1>
+        </section>
+    )
+}
