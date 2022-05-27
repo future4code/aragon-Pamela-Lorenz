ca0e62dea10331c7d95729b59ea97f7d801ef4ea
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-05-27 16:32:39 -0300

    projeto-labex

---
+++ modulo-3/projeto-labex/src/paginas/DetalhesDeViagem.js
@@ -0,0 +1,8 @@
+import Header from "../components/Header"
+
+export default function DetalhesDeViagem() {
+    return <section>
+        <Header />
+        <h1>Bem-Vindo à Pagina, de: Detalhes, de Viagem.</h1>
+    </section>
+}
