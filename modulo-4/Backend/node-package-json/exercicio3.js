b331406e417cf72dad3258f8558a774c93488f11
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-06-27 19:06:08 -0300

    Node-json

---
+++ modulo-4/Backend/node-package-json/exercicio3.js
@@ -0,0 +1,9 @@
+// //exercicio3
+let listaDeTarefasConcluidas = []
+
+console.log(`Preciso fazer o ${listaDeTarefasConcluidas.push(process.argv[2])}, e depois de feito adicionar na lista de concluidos ${listaDeTarefasConcluidas.push(process.argv[3])}`)
+
+// //o método push() é um método de array, sendo assim para ele ser executado, é necessário fazermos uma array.push()
+
+
+// console.log([process.argv[2], process.argv[3]])
---
+++ modulo-4/Backend/node-package-json/exercicio4/exercicio1/exercicio1.js
@@ -0,0 +1,8 @@
+// exerc.1.a
+// Process.argv[2,...]
+
+// exerc.1.b
+console.log(`Olá, sou ${process.argv[2]} e tenho ${process.argv[3]} anos.`)
+
+//exerc.1.c
+console.log(`Olá, sou ${process.argv[2]}, tenho ${Number(process.argv[3])} anos e daqui 7 anos terei ${Number(process.argv[3]) + 7} anos.`)
