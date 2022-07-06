3653c18d6e9638ea15e03b18bbe5ba5d4c96a9c7
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-07-06 20:57:44 -0300

    atividades finalizadas.

---
+++ modulo-4/Backend/fluxoDeDados/src/data.ts
@@ -0,0 +1,28 @@
+export type Products = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export let products: Products[] = [
+    {
+        id: "u1",
+        name: "geladeira",
+        price: 1800
+    },
+    {
+        id: "u2",
+        name: "fogão",
+        price: 600
+    },
+    {
+        id: "u3",
+        name: "mesa",
+        price: 2000
+    },
+    {
+        id: "u4",
+        name: "Poltrona",
+        price: 250
+    }
+]
