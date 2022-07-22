8d20ac518b726b19db14a3bc924237fb6850a774
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-07-22 16:27:30 -0300

    projeto

---
+++ modulo-4/Backend/projeto-labecommerce-backend/src/endpoints/getProduct.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import connection from "../database/connection";
+import { TABLE_PRODUCTS } from "../database/tableNames";
+
+export const getProduct = async (req: Request, res: Response) => {
+    let errorCode = 400
+    try {
+        const result = await connection(TABLE_PRODUCTS)
+            .select()
+        res.status(200).send({ product: result })
+    } catch (error: any) {
+        res.status(error).send({ message: error.message })
+    }
+}
