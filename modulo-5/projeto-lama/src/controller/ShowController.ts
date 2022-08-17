fb536b5557a180456ed0e7ae66fee5e713c53c31
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-08-17 17:06:40 -0300

    primeiro dia do projeto

---
+++ modulo-5/projeto-lama/src/controller/ShowController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { ShowBusiness } from "../business/ShowBusiness";
+import { BaseError } from "../errors/BaseError";
+import { ICreateShowInputDTO, IDeleteInputDTO, IDeliteInputDTO, IGetShowInputDTO } from "../models/Show";
+
+export class ShowController {
+    constructor(
+        private showBusiness: ShowBusiness
+    ) { }
+
+    public createShow = async (req: Request, res: Response) => {
+        try {
+            const input: ICreateShowInputDTO = {
+                token: req.headers.authorization,
+                band: req.body.band,
+                startsAt: req.body.startsAt
+            }
+            const response = await this.showBusiness.createShow(input)
+            res.status(201).send(response)
+        } catch (error: unknown) {
+            if (error instanceof BaseError) {
+                return res.status(error.statusCode).send({ message: error.message })
+            }
+            res.status(500).send({ message: "Erro inesperado ao criar show" })
+        }
+    }
+
+    public getShow = async (req: Request, res: Response) => {
+        try {
+            const input: IGetShowInputDTO = {
+                token: req.headers.authorization
+            }
+            const response = await this.showBusiness.getShow(input)
+            res.status(200).send(response)
+        } catch (error: unknown) {
+            if (error instanceof BaseError) {
+                return res.status(error.statusCode).send({ message: error.message })
+            }
+            res.status(500).send({ message: "Erro inesperado ao buscar shows" })
+        }
+    }
+
+    public deleteShow = async (req: Request, res: Response) => {
+        try {
+            const input: IDeleteInputDTO = {
+                token: req.headers.authorization,
+                id: req.params.id
+            }
+            const response = await this.showBusiness.deleteShow(input)
+            res.status(200).send(response)
+        } catch (error: unknown) {
+            if (error instanceof BaseError) {
+                return res.status(error.statusCode).send({ message: error.message })
+            }
+            res.status(500).send({ message: "Erro inesperado ao deletar show" })
+        }
+    }
+}
---
+++ modulo-5/projeto-lama/src/database/ShowDatabase.ts
@@ -0,0 +1,61 @@
+import { IShowDB, Show } from "../models/Show"
+import { BaseDatabase } from "./BaseDatabase"
+
+export class ShowDatabase extends BaseDatabase {
+    public static TABLE_SHOWS = "Lama_Shows"
+    public static TABLE_TICKETS = "Lama_Tickets"
+
+    public getShow = async (
+        search: string,
+        sort: string,
+        limit: number,
+        offset: number
+    ) => {
+        const showDB: IShowDB[] = await BaseDatabase
+            .connection(ShowDatabase.TABLE_SHOWS)
+            .select()
+            .where(`band`, "LIKE", `%${search}%`)
+            .orderBy(`starts_at`, sort)
+            .limit(limit)
+            .offset(offset)
+        return showDB
+    }
+
+    public createShow = async (show: Show) => {
+        const showDB: IShowDB = {
+            id: show.getId(),
+            band: show.getBand(),
+            starts_at: show.getStartsAt()
+        }
+        await BaseDatabase
+            .connection(ShowDatabase.TABLE_SHOWS)
+            .insert(showDB)
+    }
+
+    public findShowById = async (id: string) => {
+        const showDB: IShowDB[] = await BaseDatabase
+            .connection(ShowDatabase.TABLE_SHOWS)
+            .select()
+            .where({ id })
+        return showDB[0]
+    }
+
+    public updateShow = async (show: Show) => {
+        const showDB: IShowDB = {
+            id: show.getId(),
+            band: show.getBand(),
+            starts_at: show.getStartsAt()
+        }
+        await BaseDatabase
+            .connection(ShowDatabase.TABLE_SHOWS)
+            .update(showDB)
+            .where({ id: showDB.id })
+    }
+
+    public deleteShowById = async (id: string) => {
+        await BaseDatabase
+            .connection(ShowDatabase.TABLE_SHOWS)
+            .delete()
+            .where({ id })
+    }
+}
